1705f1128f92c89f2af27da8ce6aa6f2633d1cc8
Florian Hartung <florian.hartung@dlr.de>
2024-02-19 14:09:21 +0100

    fix(fuel_tank): add empty periodic process
    P4 requires at least one periodic or aperiodic process to exist

--- examples/fuel_tank_controller/src/main.rs
+++ examples/fuel_tank_controller/src/main.rs
@@ -2,8 +2,9 @@
 
 use a653rs::partition;
 use a653rs::prelude::PartitionExt;
-use a653rs_linux::partition::ApexLogger;
 use log::LevelFilter;
+
+use a653rs_linux::partition::ApexLogger;
 
 fn main() {
     ApexLogger::install_panic_hook();
@@ -14,7 +15,6 @@ fn main() {
 
 #[partition(a653rs_linux::partition::ApexLinuxPartition)]
 mod hello {
-
     use a653rs::prelude::SystemTime;
 
     #[sampling_in(name = "fuel_sensors", msg_size = "10KB", refresh_period = "10s")]
@@ -22,12 +22,27 @@ mod hello {
 
     #[sampling_out(name = "fuel_actuators", msg_size = "10KB")]
     struct FuelActuators;
-
     #[start(cold)]
-    fn cold_start(_ctx: start::Context) {}
+    fn cold_start(mut ctx: start::Context) {
+        ctx.create_periodic().unwrap().start().unwrap();
+    }
 
     #[start(warm)]
     fn warm_start(ctx: start::Context) {
         cold_start(ctx)
+    }
+
+    #[periodic(
+        period = "0ms",
+        time_capacity = "Infinite",
+        stack_size = "100KB",
+        base_priority = 1,
+        deadline = "Soft"
+    )]
+    fn periodic(ctx: periodic::Context) {
+        loop {
+            // Empty for now
+            ctx.periodic_wait().unwrap();
+        }
     }
 }
